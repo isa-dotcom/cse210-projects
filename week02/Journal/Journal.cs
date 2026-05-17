c20d087293d167e5d36101bbdd75565b4f58e17c
isa-dotcom <idacunha@byupathway.edu>
2026-05-17 15:27:03 -0300

    Completed Journal Program with creativity features

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        Console.WriteLine($"You have {_entries.Count} journal entries.");
+        Console.WriteLine();
+
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string file)
+    {
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"{entry._date}|{entry._mood}|{entry._promptText}|{entry._entryText}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string file)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("|");
+
+            Entry entry = new Entry();
+
+            entry._date = parts[0];
+            entry._mood = parts[1];
+            entry._promptText = parts[2];
+            entry._entryText = parts[3];
+
+            _entries.Add(entry);
+        }
+    }
+}
